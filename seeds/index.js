ae044fb7a7bc44dc0245520fe3803b212504ea45
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-04-20 18:45:58 -0700

    added styling

--- seeds/index.js
+++ seeds/index.js
@@ -21,10 +21,14 @@ db.once("open", ()=>{
 const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++ ){
-       const rand = Math.floor((Math.random()*1000)+1);
+       const rand = Math.floor(Math.random()*1000);
+       const price = Math.floor((Math.random()*20)+10);
        const camp = new Campground({
             location: `${cities[rand].city}, ${cities[rand].state}`,
-            title:`${sample(descriptors)}, ${sample(places)}`
+            title:`${sample(descriptors)}, ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            description: "Cool Image",
+            price
         })
         await camp.save();
     }
