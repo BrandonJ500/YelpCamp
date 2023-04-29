499d81349b9876f6437b79bbcff33ae5ab894ab9
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-04-29 15:54:50 -0700

    added validation schema with joi

--- app.js
+++ app.js
@@ -28,7 +28,24 @@ app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }));
 
 
-
+const validateCampground = (req,res,next)=> {
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el=>el.message).join(",")
+        throw new ExpressError(msg, 400)
+    }else {
+        next();
+    }
+}
 
 app.use((req, res, next) => {
 
@@ -48,7 +65,7 @@ app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
 })
 
-app.post("/campgrounds", catchAsync(async (req, res, next) => {
+app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError("Invalid Campground data", 400);
     const campground = new Campground(req.body.campground)
     await campground.save();
@@ -65,7 +82,7 @@ app.get("/campgrounds/:id/edit", catchAsync(async (req, res, next) => {
     res.render("campgrounds/edit", { campground });
 }))
 
-app.put("/campgrounds/:id", catchAsync(async (req, res, next) => {
+app.put("/campgrounds/:id",validateCampground, catchAsync(async (req, res, next) => {
     const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 }))
